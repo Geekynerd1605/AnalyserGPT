830c70259223d9d610813e60ac167b755e2fe6b3
Swastika Dalmia <geekynerd1605@gmail.com>
2026-05-19 07:16:33 +0530

    streamlit changes

--- config/constants.py
+++ config/constants.py
@@ -1,3 +1,4 @@
 TIMEOUT_DOCKER=120
-WORK_DIR_DOCKER="/home/hb/Desktop/AnalyserGPT/tmp"
+WORK_DIR_DOCKER="/Users/swastikadalmia/Desktop/AI Projects/AutoGen Projects/AnalyserGPT/tmp"
+# WORK_DIR_DOCKER="/home/hb/Desktop/AnalyserGPT/tmp"
 MODEL_NAME="gpt-4o"
