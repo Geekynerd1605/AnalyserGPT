99be2441cae6ddda3b08f8b1b42e498cd93a19f6
Swastika Dalmia <geekynerd1605@gmail.com>
2026-05-19 07:15:30 +0530

    streamlit changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ from teams.analyzer_gpt import GetDataAnalyzerTeam
 from models.openai_model_client import get_model_client
 from config.docker_util import getDockerCommandLineCodeExecutor, start_docker_container, stop_docker_container
 from autogen_agentchat.messages import TextMessage
+from dotenv import load_dotenv
+load_dotenv()
 
 st.title("Analyser GPT - Digital Data Analyzer")
 uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
@@ -30,8 +32,19 @@ async def run_analyzer_gpt(docker, model_client, task):
 
 if task:
     if uploaded_file is not None:
+        if not os.path.exists('tmp'):
+            os.makedirs('tmp')
+
         with open('tmp/data.csv', 'wb') as f:
             f.write(uploaded_file.getvalue())
-        pass
+
+        openai_model_client=get_model_client()
+        docker=getDockerCommandLineCodeExecutor()
+
+        error = asyncio.run(run_analyzer_gpt(docker, openai_model_client, task))
+        if error:
+            st.error(f"An error occurred: {error}")
+        else:
+            st.success("Analysis completed successfully")
     else:
         st.warning("Please upload a CSV file and enter a task")
